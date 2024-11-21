79c50d0dc63c92585a7aa50c85ec94239c4408cf
annechh <142426482+annechh@users.noreply.github.com>
2024-11-21 16:38:31 +0100

    Adds export const page paths

---
+++ src/js/utilities/pagePaths.js
@@ -0,0 +1,17 @@
+export const homePage = '/';
+
+export const authPage = '/pages/auth/';
+
+export const loginPage = '/pages/auth/login/';
+
+export const registerPage = '/pages/auth/register/';
+
+export const listingPage = '/pages/listing/';
+
+export const newListingPage = '/pages/listing/create/';
+
+export const editListingPage = '/pages/listing/create/';
+
+export const profilePage = '/pages/profile/';
+
+export const searchPage = '/pages/search/';
