703ce8eab9609101ecc832c4182923ef6bd8dd98
annechh <142426482+annechh@users.noreply.github.com>
2024-11-23 15:07:49 +0100

    Imports styleBody, styleMain, buildHeader, fontawsomeScript, clickMenu and buildFooter functions to app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 import './css/style.css';
 
 import router from './js/router';
+import { buildFooter } from './js/ui/dom/footer';
+import { buildHeader, clickMenu } from './js/ui/dom/header';
+import { styleBody, styleMain } from './js/ui/styles/styles';
+import { fontawsomeScript } from './js/utilities/fontawsome';
 
 async function app() {
   await router(window.location.pathname);
+  styleBody();
+  styleMain();
+
+  buildHeader();
+  fontawsomeScript();
+  clickMenu();
+  buildFooter();
 }
 
 app();
