bca2281e701d83e111fa2281cb1754b635e32dea
annechh <142426482+annechh@users.noreply.github.com>
2024-11-24 16:57:07 +0100

    Imports onRegister and adds eventListener to handle form submit

---
+++ src/js/router/views/register.js
@@ -0,0 +1,7 @@
+import { onRegister } from '../../ui/auth/register';
+async function handleRegister() {
+  const form = document.forms.register;
+  form.addEventListener('submit', onRegister);
+}
+
+handleRegister();
