2121911ac464927a42bd71c65815e2beb4271906
annechh <142426482+annechh@users.noreply.github.com>
2024-11-24 05:31:25 +0100

    Updated import name after filename change for styleForms

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import './css/style.css';
 import router from './js/router';
 import { buildFooter } from './js/ui/dom/footer';
 import { buildHeader, clickMenu } from './js/ui/dom/header';
-import { styleBody, styleMain } from './js/ui/styles/styles';
+import { styleBody, styleMain } from './js/ui/styles/styleForms';
 import { fontawsomeScript } from './js/utilities/fontawsome';
 
 async function app() {
