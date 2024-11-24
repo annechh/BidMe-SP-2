cfc239c5700815eb2b4483c1ca7f98d135eff5c6
annechh <142426482+annechh@users.noreply.github.com>
2024-11-24 05:24:07 +0100

    Adds styles to forms with functions

---
+++ src/js/ui/styles/styleForms.js
@@ -0,0 +1,80 @@
+export function styleBody() {
+  const body = document.querySelector('body');
+  body.classList.add('flex', 'flex-col', 'items-center', 'min-h-screen');
+}
+
+export function styleMain() {
+  const main = document.querySelector('main');
+  main.classList.add(
+    'flex',
+    'flex-grow',
+    'w-full',
+    'flex-col',
+    'items-center',
+    'justify-center'
+  );
+}
+
+export function styleFormDivWrapper() {
+  const formDiv = document.querySelector('.form-div-wrapper');
+  formDiv.classList.add(
+    'bg-red-200',
+    'dark:bg-red-200',
+    'py-10',
+    'md:py-20',
+    'w-full',
+    'flex',
+    'flex-col',
+    'gap-5',
+    'justify-center',
+    'items-center',
+    'mx-4'
+  );
+}
+
+export function styleForm() {
+  const form = document.querySelector('.form');
+  form.classList.add(
+    'flex',
+    'flex-col',
+    'gap-[25px]',
+    'max-w-[600px]',
+    'w-full',
+    'h-full'
+  );
+}
+
+export function styleLabelInputWrapper() {
+  const targetDiv = document.querySelectorAll('.label-input-wrapper');
+
+  targetDiv.forEach((div) => {
+    div.classList.add('flex', 'flex-col', 'gap-2', 'mx-4');
+  });
+}
+
+export function styleLabel() {
+  const targetLabel = document.querySelectorAll('.label');
+  console.log(targetLabel);
+
+  targetLabel.forEach((input) => {
+    input.classList.add('text-white', 'text-xs', 'md:text-base');
+  });
+}
+
+export function styleInput() {
+  const targetInput = document.querySelectorAll('.input');
+  console.log(targetInput);
+
+  targetInput.forEach((input) => {
+    input.classList.add(
+      'rounded',
+      'h-[50px]',
+      'px-4',
+      'text-black',
+      'font-semibold',
+      'bg-white',
+      'border',
+      'border-darkFaded'
+    );
+  });
+}
