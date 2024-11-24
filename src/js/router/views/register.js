c183423c97e92de44f89bc777625eb4dee470679
annechh <142426482+annechh@users.noreply.github.com>
2024-11-24 16:57:26 +0100

    Import form styles from styleForms.js

--- src/js/router/views/register.js
+++ src/js/router/views/register.js
@@ -1,5 +1,19 @@
 import { onRegister } from '../../ui/auth/register';
+import {
+  styleForm,
+  styleFormDivWrapper,
+  styleInput,
+  styleLabel,
+  styleLabelInputWrapper,
+} from '../../ui/styles/styleForms';
+
 async function handleRegister() {
+  styleForm();
+  styleLabelInputWrapper();
+  styleLabel();
+  styleInput();
+  styleFormDivWrapper();
+
   const form = document.forms.register;
   form.addEventListener('submit', onRegister);
 }
