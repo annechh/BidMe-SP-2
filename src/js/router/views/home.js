85e8e6d02126b86e5cda3a100f7c35da0491f2cb
annechh <142426482+annechh@users.noreply.github.com>
2024-11-26 20:26:45 +0100

    Import welcomeUser function

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,8 +1,10 @@
 import { readListings } from '../../api/listing/read';
 import { renderListingCards } from '../../ui/dom/home/listingsHome';
+import { welcomeUser } from '../../ui/dom/home/welcomeMessage';
 
 async function loadHomePage() {
   const { listings } = await readListings();
   renderListingCards(listings);
+  welcomeUser();
 }
 loadHomePage();
