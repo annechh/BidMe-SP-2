4df25b558771cc37cb1d8181a7b23f3ac88e4703
annechh <142426482+annechh@users.noreply.github.com>
2024-11-29 23:10:16 +0100

    Import buildListingsWins

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,10 +1,12 @@
 import { profileData } from '../../ui/dom/profile/profileUserData';
 import { buildListingCardsProfile } from '../../ui/dom/profile/profileAuctions';
+import { buildListingsWins } from '../../ui/dom/profile/profileWins';
 
 async function loadProfile() {
   await profileData();
 
   await buildListingCardsProfile();
+  await buildListingsWins();
 }
 
 loadProfile();
