273941f075b165357c14eba5701fec3bda9da1a2
annechh <142426482+annechh@users.noreply.github.com>
2024-11-29 23:05:21 +0100

    Adds fetch profileListingsWins

--- src/js/api/profile/read.js
+++ src/js/api/profile/read.js
@@ -4,7 +4,7 @@ import { headers } from '../headers';
 export async function readProfileName(name) {
   try {
     const url = `${API_PROFILES}/${name}?&_listings=true&_wins=true&_bids=true&sort=created&sortOrder=desc`;
-    console.log(url);
+    // console.log(url);
 
     const response = await fetch(url, {
       method: 'GET',
@@ -28,8 +28,8 @@ export async function readProfileName(name) {
 
 export async function readProfileListings(name) {
   try {
-    const url = `${API_PROFILES}/${name}/listings?&_listings=true&_wins=true&_bids=true&sort=created&sortOrder=desc`;
-    console.log(url);
+    const url = `${API_PROFILES}/${name}/listings?&_seller=true&_bids=true&sort=created&sortOrder=desc`;
+    // console.log(url);
 
     const response = await fetch(url, {
       method: 'GET',
@@ -51,9 +51,34 @@ export async function readProfileListings(name) {
   }
 }
 
-// export async function readProfiles() {
+export async function readListingsWins(name) {
+  try {
+    const url = `${API_PROFILES}/${name}/wins?&_wins=true&_bids=true&sort=created&sortOrder=desc`;
+    console.log(url);
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: headers(),
+    });
+
+    if (!response.ok) {
+      alert('Could not get profile');
+    } else {
+      const data = await response.json();
+      const profile = data.data;
+
+      console.log('Read profile wins: ', data.data);
+
+      return { profile };
+    }
+  } catch (error) {
+    alert(error, 'Error loading profile');
+  }
+}
+
+// export async function readAllProfiles() {
 //   try {
-//     const url = `${API_PROFILES}`;
+//     const url = `${API_PROFILES}?&_wins=true&_&_listings=true&sort=created&sortOrder=desc`;
 //     console.log(url);
 
 //     const response = await fetch(url, {
