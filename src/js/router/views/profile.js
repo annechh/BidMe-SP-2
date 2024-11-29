67082600c773a391535d4417a420814d7a8a38a2
annechh <142426482+annechh@users.noreply.github.com>
2024-11-29 23:51:09 +0100

    Import formStyle

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,12 +1,14 @@
 import { profileData } from '../../ui/dom/profile/profileUserData';
 import { buildListingCardsProfile } from '../../ui/dom/profile/profileAuctions';
 import { buildListingsWins } from '../../ui/dom/profile/profileWins';
+import { formStyle } from '../../ui/styles/styleForms';
 
 async function loadProfile() {
   await profileData();
-
   await buildListingCardsProfile();
   await buildListingsWins();
+
+  formStyle();
 }
 
 loadProfile();
