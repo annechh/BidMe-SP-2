261c6c07258f0d16e0d575e804ad6a63e1f58a7a
annechh <142426482+annechh@users.noreply.github.com>
2024-12-03 00:16:54 +0100

    Moved onUpdateProfile function to ui/profile/update from ui/DOM/profile/profileEdit

---
+++ src/js/ui/profile/update.js
@@ -0,0 +1,18 @@
+import { updateProfile } from '../../api/profile/update';
+
+export async function onUpdateProfile(event) {
+  event.preventDefault();
+
+  const form = new FormData(event.target);
+
+  const banner = form.get('banner');
+  const avatar = form.get('avatar');
+
+  const getForms = {
+    banner: banner ? { url: banner } : null,
+    avatar: avatar ? { url: avatar } : null,
+    bio: form.get('bio'),
+  };
+
+  await updateProfile(getForms);
+}
