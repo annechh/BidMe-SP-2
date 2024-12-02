8bc409fab2ea6ef3577d176a33d7eeb36a6b39c7
annechh <142426482+annechh@users.noreply.github.com>
2024-12-02 21:09:04 +0100

    Adds clear field handling for edit profile

--- src/js/ui/dom/profile/profileEdit.js
+++ src/js/ui/dom/profile/profileEdit.js
@@ -19,6 +19,14 @@ export async function onUpdateProfile(event) {
   await updateProfile(getForms);
 }
 
+const bannerInput = document.getElementById('banner');
+const avatarInput = document.getElementById('avatar');
+const bioInput = document.getElementById('bio');
+
+const clearBannerButton = document.getElementById('clearBannerField');
+const clearAvatarButton = document.getElementById('clearAvatarField');
+const clearBioButton = document.getElementById('clearBioField');
+
 export async function viewProfileData() {
   const userData = getLocalStorage();
   let profileData;
@@ -29,10 +37,48 @@ export async function viewProfileData() {
   }
 
   if (profileData) {
-    document.getElementById('banner').value =
-      profileData.profile.banner?.url || '';
-    document.getElementById('avatar').value =
-      profileData.profile.avatar?.url || '';
-    document.getElementById('bio').value = profileData.profile.bio || '';
+    bannerInput.value = profileData.profile.banner?.url || '';
+    avatarInput.value = profileData.profile.banner?.url || '';
+    bioInput.value = profileData.profile.bio || '';
+
+    clearBannerButton.style.display = bannerInput.value ? 'block' : 'none';
+    clearAvatarButton.style.display = bannerInput.value ? 'block' : 'none';
+    clearBioButton.style.display = bannerInput.value ? 'block' : 'none';
   }
 }
+
+bannerInput.addEventListener('input', () => {
+  clearBannerButton.style.display = bannerInput.value ? 'block' : 'none';
+});
+avatarInput.addEventListener('input', () => {
+  clearAvatarButton.style.display = avatarInput.value ? 'block' : 'none';
+});
+bioInput.addEventListener('input', () => {
+  clearBioButton.style.display = bioInput.value ? 'block' : 'none';
+});
+
+clearBannerButton.addEventListener('click', () => {
+  bannerInput.value = '';
+  clearBannerButton.style.display = 'none';
+});
+clearAvatarButton.addEventListener('click', () => {
+  avatarInput.value = '';
+  clearAvatarButton.style.display = 'none';
+});
+clearBioButton.addEventListener('click', () => {
+  bioInput.value = '';
+  clearBioButton.style.display = 'none';
+});
+
+const getForm = document.getElementById('formDivWrapper');
+const editProfile = document.getElementById('profileEdit');
+
+editProfile.addEventListener('click', () => {
+  if (getForm.classList.contains('hidden')) {
+    getForm.classList.remove('hidden');
+    getForm.classList.add('flex');
+  } else {
+    getForm.classList.remove('flex');
+    getForm.classList.add('hidden');
+  }
+});
