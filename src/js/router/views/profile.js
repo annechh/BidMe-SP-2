afc1faf233e5681b65af4d8aa0d17d7acea20f9d
annechh <142426482+annechh@users.noreply.github.com>
2024-12-03 00:19:48 +0100

    Update path for onUpdateProfile import

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -2,10 +2,8 @@ import { profileData } from '../../ui/dom/profile/profileUserData';
 import { buildListingCardsProfile } from '../../ui/dom/profile/profileAuctions';
 import { buildListingsWins } from '../../ui/dom/profile/profileWins';
 import { formStyle } from '../../ui/styles/styleForms';
-import {
-  onUpdateProfile,
-  viewProfileData,
-} from '../../ui/dom/profile/profileEdit';
+import { viewProfileData } from '../../ui/dom/profile/profileEdit';
+import { onUpdateProfile } from '../../ui/profile/update';
 
 async function loadProfile() {
   await profileData();
