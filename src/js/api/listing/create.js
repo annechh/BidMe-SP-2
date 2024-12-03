edb37bd0e672a9aa58b577d6ebfc44f5b3950ed0
annechh <142426482+annechh@users.noreply.github.com>
2024-12-03 22:53:17 +0100

    Adds createListing function to handle post request

---
+++ src/js/api/listing/create.js
@@ -0,0 +1,37 @@
+import { LISTING_PAGE } from '../../utilities/pagePaths';
+import { showSuccessMessage } from '../../utilities/validation';
+import { API_LISTINGS } from '../endpoints';
+import { headers } from '../headers';
+
+export async function createListing({
+  title,
+  description,
+  tags,
+  endsAt,
+  media,
+}) {
+  try {
+    const response = await fetch(API_LISTINGS, {
+      method: 'POST',
+      headers: headers(),
+      body: JSON.stringify({ title, description, tags, endsAt, media }),
+    });
+    console.log(response.body);
+
+    if (!response.ok) {
+      alert('Failed to create listing');
+    } else {
+      const data = await response.json();
+      console.log('Response data: ', data.data);
+
+      showSuccessMessage();
+
+      setTimeout(() => {
+        window.location.href = `${LISTING_PAGE}/?id=${data.data.id}`;
+      }, 2500);
+      return data.data;
+    }
+  } catch (error) {
+    alert(error, 'Failed to create new listing');
+  }
+}
