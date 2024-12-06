4ca2c052e0463ca7b6b2a25396a0d45806d18e16
annechh <142426482+annechh@users.noreply.github.com>
2024-12-06 19:06:25 +0100

    Adds handleSearch function to handle search events

--- src/js/ui/search/search.js
+++ src/js/ui/search/search.js
@@ -1,12 +1,67 @@
 import { searchListings } from '../../api/search/search';
+import { renderListingCards } from '../dom/home/listingsCardsHome';
 
 export async function onSearchListing(event) {
   event.preventDefault();
 
   const form = new FormData(event.target);
-
   const search = form.get('search');
-  console.log(search);
 
-  await searchListings(search);
+  return search;
+}
+
+const form = document.forms.searchForm;
+const displaySearchWord = document.getElementById('searchWords');
+const resetButton = document.getElementById('resetSearch');
+const searchInput = document.getElementById('search');
+const searchButton = document.getElementById('searchButton');
+
+export async function handleSearch(listings) {
+  toggleButtonState();
+  resetToAllListings(listings);
+  console.log('Listing in handleSearch', listings);
+
+  searchInput.addEventListener('input', toggleButtonState);
+
+  form.addEventListener('submit', async (event) => {
+    const onSearch = await onSearchListing(event);
+    const searchListing = await searchListings(onSearch);
+
+    if (searchListing) {
+      displaySearchWord.classList.remove('hidden');
+      displaySearchWord.classList.add('flex');
+    } else {
+      renderListingCards([]);
+      displaySearchWord.classList.remove('flex');
+      displaySearchWord.classList.add('hidden');
+    }
+
+    renderListingCards(searchListing.listings);
+    console.log('render listing cards on search', searchListing.listings);
+  });
+
+  searchInput.addEventListener('input', () => {
+    if (!searchInput.value.trim()) {
+      resetToAllListings(listings);
+    }
+  });
+
+  resetButton.addEventListener('click', () => {
+    resetToAllListings(listings);
+    toggleButtonState();
+  });
+}
+
+const toggleButtonState = () => {
+  const isEmpty = !searchInput.value.trim();
+  searchButton.disabled = isEmpty;
+  searchButton.classList.toggle('cursor-not-allowed', isEmpty);
+  searchButton.classList.toggle('cursor-pointer', !isEmpty);
+};
+
+function resetToAllListings(listings) {
+  renderListingCards(listings);
+  displaySearchWord.classList.add('hidden');
+  form.reset();
+  toggleButtonState();
 }
