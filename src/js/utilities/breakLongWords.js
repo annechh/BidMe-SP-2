9b67eae57d94a51af5a6a705a230c5da30d0cbcc
annechh <142426482+annechh@users.noreply.github.com>
2024-12-10 17:02:47 +0100

    Adds applyBreakWordClass function

---
+++ src/js/utilities/breakLongWords.js
@@ -0,0 +1,14 @@
+export function applyBreakWordClass(element) {
+  const words = element.textContent.split(' ');
+  const hasLongWord = words.some((word) => word.length > 30);
+
+  if (hasLongWord) {
+    element.classList.add('break-all');
+  } else {
+    element.classList.add(
+      'break-words',
+      'overflow-wrap-anywhere',
+      'hyphens-auto'
+    );
+  }
+}
