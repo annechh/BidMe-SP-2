04407e741c4b47997a21fdad8182cc48ae46ba59
annechh <142426482+annechh@users.noreply.github.com>
2024-12-10 22:35:11 +0100

    Import carousel for home page

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,5 +1,6 @@
 import { readListings } from '../../api/listing/read';
-import { buildLastChanceListings } from '../../ui/dom/home/lastChanceListings';
+import { carousel } from '../../ui/dom/home/carousel';
+// import { buildLastChanceListings } from '../../ui/dom/home/lastChanceListings';
 import { renderListingCards } from '../../ui/dom/home/listingsCardsHome';
 import { welcomeUser } from '../../ui/dom/home/welcomeMessage';
 import { handleSearch } from '../../ui/search/search';
@@ -7,15 +8,23 @@ import { styleMain } from '../../ui/styles/styleForms';
 
 async function loadHomePage() {
   const { listings: allListings } = await readListings({ limit: 50 });
-  const { listings: topListings } = await readListings({
+  const { listings: endsSoonListings } = await readListings({
     limit: 3,
     sort: 'endsAt',
     sortOrder: 'asc',
   });
   await handleSearch(allListings);
 
+  try {
+    const getCarousel = document.getElementById('carousel');
+    getCarousel.classList.remove('animate-pulse');
+    await carousel(endsSoonListings);
+  } catch (error) {
+    throw new error();
+  }
+
   renderListingCards(allListings);
-  buildLastChanceListings(topListings);
+  // buildLastChanceListings(topListings);
   welcomeUser();
   styleMain();
 }
