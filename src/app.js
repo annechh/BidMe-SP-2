1bc8b2c5d0dc599eac2ee51510384675dbd24550
annechh <142426482+annechh@users.noreply.github.com>
2024-12-12 00:18:18 +0100

    Update path for styleBody and styleMain imports

--- src/app.js
+++ src/app.js
@@ -4,18 +4,18 @@ import router from './js/router';
 import { buildFooter } from './js/ui/dom/footer';
 import { buildHeader, clickMenu } from './js/ui/dom/header';
 import { setLogoutListener } from './js/ui/global/logout';
-import { styleBody, styleMain } from './js/ui/styles/styleForms';
+import { styleBody, styleMain } from './js/ui/styles/styleElement';
 import { fontawsomeScript } from './js/utilities/fontawsome';
 
 async function app() {
   await router(window.location.pathname);
+  await buildHeader();
+
   styleBody();
   styleMain();
-
-  await buildHeader();
   fontawsomeScript();
-  clickMenu();
   buildFooter();
+  clickMenu();
   setLogoutListener();
 }
 
