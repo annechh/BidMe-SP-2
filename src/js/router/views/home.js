e04c9834608d05b6b6d1db124460c4d3ebca8e14
annechh <142426482+annechh@users.noreply.github.com>
2024-12-11 14:36:57 +0100

    Adds remove animate pulse when carousel is done loading

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -9,22 +9,22 @@ import { styleMain } from '../../ui/styles/styleForms';
 async function loadHomePage() {
   const { listings: allListings } = await readListings({ limit: 50 });
   const { listings: endsSoonListings } = await readListings({
-    limit: 3,
+    limit: 6,
     sort: 'endsAt',
     sortOrder: 'asc',
   });
   await handleSearch(allListings);
 
   try {
-    const getCarousel = document.getElementById('carousel');
-    getCarousel.classList.remove('animate-pulse');
+    const animatePulseLoader = document.querySelector('.loader');
+    animatePulseLoader.classList.remove('animate-pulse');
     await carousel(endsSoonListings);
   } catch (error) {
     throw new error();
   }
 
   renderListingCards(allListings);
-  // buildLastChanceListings(topListings);
+  // buildLastChanceListings(endsSoonListings);
   welcomeUser();
   styleMain();
 }
