7686854f9ed490f0c80f3c9ab772fd96281c3add
annechh <142426482+annechh@users.noreply.github.com>
2024-12-12 00:21:59 +0100

    Relocated styleBody and styleMain to styleElements.js

--- src/js/ui/styles/styleForms.js
+++ src/js/ui/styles/styleForms.js
@@ -1,29 +1,3 @@
-export function styleBody() {
-  const body = document.querySelector('body');
-  body.classList.add(
-    'flex',
-    'flex-col',
-    'items-center',
-    'min-h-screen',
-    'font-roboto'
-  );
-}
-
-export function styleMain() {
-  const main = document.querySelector('main');
-  main.classList.add(
-    'flex',
-    'flex-grow',
-    'w-full',
-    'flex-col',
-    'items-center',
-    'justify-center',
-    'mt-[50px]',
-    'md:mt-[60px]',
-    'lg:mt-[75px]'
-  );
-}
-
 export function styleFormDivWrapper() {
   const formDiv = document.querySelector('.form-div-wrapper');
   formDiv.classList.add(
