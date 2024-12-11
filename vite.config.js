f770144ef5c4069846504fcc084002c1e5176eff
annechh <142426482+annechh@users.noreply.github.com>
2024-12-11 21:53:37 +0100

    Removed edit listing from vite config

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig({
         register: resolve(__dirname, './pages/auth/register/index.html'),
         profile: resolve(__dirname, './pages/profile/index.html'),
         listing: resolve(__dirname, './pages/listing/index.html'),
-        editListing: resolve(__dirname, './pages/listing/edit/index.html'),
         createListing: resolve(__dirname, './pages/listing/create/index.html'),
         searchListing: resolve(__dirname, './pages/search/index.html'),
       },
