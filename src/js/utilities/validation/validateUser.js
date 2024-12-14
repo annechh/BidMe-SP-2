2a22ba16dfd5a18d0c3a65e02238e35b86570fb4
annechh <142426482+annechh@users.noreply.github.com>
2024-12-14 02:45:04 +0100

    Moved validate for name, email and password to validateUser.js

---
+++ src/js/utilities/validation/validateUser.js
@@ -0,0 +1,75 @@
+import { checkIfEmptyField } from './checkEmptyFields';
+
+function nameValidation(name, nameError) {
+  const namePrefix = /^[\w]{1,20}$/;
+  if (!namePrefix.test(name)) {
+    nameError.textContent =
+      'Username can only contain letters, numbers, and underscores.';
+    nameError.classList.remove('hidden');
+    return false;
+  } else {
+    nameError.textContent = '';
+    nameError.classList.add('hidden');
+    return true;
+  }
+}
+
+export function validateName() {
+  const nameInput = document.getElementById('name');
+  const nameError = document.getElementById('nameError');
+  // nameError.classList.add('py-2', 'px-5', 'bg-delete');
+  const name = nameInput.value;
+
+  if (checkIfEmptyField(name, nameError)) {
+    return false;
+  }
+  return nameValidation(name, nameError);
+}
+
+function emailValidation(email, emailError) {
+  const emailPrefix = /^[\w\-.]+@(stud\.)?noroff\.no$/;
+  if (!emailPrefix.test(email)) {
+    emailError.textContent =
+      'Not a valid email address. Must contain @stud.noroff.no or @noroff.no.';
+    emailError.classList.remove('hidden');
+    return false;
+  }
+  emailError.textContent = '';
+  emailError.classList.add('hidden');
+  return true;
+}
+
+export function validateEmail() {
+  const emailInput = document.getElementById('email');
+  const emailError = document.getElementById('emailError');
+  // emailError.classList.add('py-2', 'px-5', 'bg-delete');
+  const email = emailInput.value;
+
+  if (checkIfEmptyField(email, emailError)) {
+    return false;
+  }
+  return emailValidation(email, emailError);
+}
+
+function passwordValidation(password, passwordError) {
+  if (password.length < 8) {
+    passwordError.textContent = 'Password needs to be at least 8 characters.';
+    passwordError.classList.remove('hidden');
+    return false;
+  }
+  passwordError.textContent = '';
+  passwordError.classList.add('hidden');
+  return true;
+}
+
+export function validatePassword() {
+  const passwordInput = document.getElementById('password');
+  const passwordError = document.getElementById('passwordError');
+  // passwordError.classList.add('py-2', 'px-5', 'bg-delete');
+  const password = passwordInput.value;
+
+  if (checkIfEmptyField(password, passwordError)) {
+    return false;
+  }
+  return passwordValidation(password, passwordError);
+}
