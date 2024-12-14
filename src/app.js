28bebb731e1488945c4735c41005cc95fd828d57
annechh <142426482+annechh@users.noreply.github.com>
2024-12-14 22:00:53 +0100

    Removed styleElement.js import

--- src/app.js
+++ src/app.js
@@ -4,14 +4,11 @@ import router from './js/router';
 import { buildFooter } from './js/ui/dom/footer';
 import { buildHeader, clickMenu } from './js/ui/dom/header';
 import { setLogoutListener } from './js/ui/global/logout';
-import { styleBody, styleMain } from './js/ui/styles/styleElement';
 
 async function app() {
   await router(window.location.pathname);
   await buildHeader();
 
-  styleBody();
-  styleMain();
   buildFooter();
   clickMenu();
   setLogoutListener();
