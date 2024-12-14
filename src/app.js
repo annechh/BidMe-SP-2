58ceab26587231b16ab55f4c92767e5c2c4af5b6
annechh <142426482+annechh@users.noreply.github.com>
2024-12-14 21:48:18 +0100

    Removed fontawsome import

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import { buildFooter } from './js/ui/dom/footer';
 import { buildHeader, clickMenu } from './js/ui/dom/header';
 import { setLogoutListener } from './js/ui/global/logout';
 import { styleBody, styleMain } from './js/ui/styles/styleElement';
-import { fontawsomeScript } from './js/utilities/fontawsome';
 
 async function app() {
   await router(window.location.pathname);
@@ -13,7 +12,6 @@ async function app() {
 
   styleBody();
   styleMain();
-  fontawsomeScript();
   buildFooter();
   clickMenu();
   setLogoutListener();
