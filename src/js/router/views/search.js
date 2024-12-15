e1775f3625e4f43dd857e4f86401fc88395fdc53
annechh <142426482+annechh@users.noreply.github.com>
2024-12-15 01:41:21 +0100

    Deleted search view js file

--- src/js/router/views/search.js
+++
@@ -1,8 +0,0 @@
-import { onSearchListing } from '../../ui/search/search';
-
-async function loadSearchPage() {
-  const form = document.forms.searchForm;
-  form.addEventListener('submit', (event) => onSearchListing(event));
-}
-
-loadSearchPage();
