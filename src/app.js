e4954d71656df09ce346d5440eb49a2fe4432753
annechh <142426482+annechh@users.noreply.github.com>
2024-12-15 03:36:35 +0100

    Removed footer.js import

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import './css/style.css';
 
 import router from './js/router';
-import { buildFooter } from './js/ui/dom/footer';
 import { buildHeader, clickMenu } from './js/ui/dom/header';
 import { setLogoutListener } from './js/ui/global/logout';
 
@@ -9,7 +8,6 @@ async function app() {
   await router(window.location.pathname);
   await buildHeader();
 
-  buildFooter();
   clickMenu();
   setLogoutListener();
 }
