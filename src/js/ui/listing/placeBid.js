6129c636f4950983140fe867fefc00b562ed4364
annechh <142426482+annechh@users.noreply.github.com>
2025-06-06 14:44:25 +0200

    fix: removed alert and added bid validation and error messages

--- src/js/ui/listing/placeBid.js
+++ src/js/ui/listing/placeBid.js
@@ -1,11 +1,15 @@
 import { placeBidListing } from '../../api/listing/placeBid';
+import { checkIfEmptyField } from '../../utilities/validation/checkEmptyFields';
 
 export async function onPlaceBid(event) {
   event.preventDefault();
 
   const form = new FormData(event.target);
-
   const placeBid = Number(form.get('bid'));
+
+  const errorMessage = document.querySelector('#bidError');
+  const isEmpty = checkIfEmptyField(placeBid, errorMessage);
+  if (isEmpty) return;
 
   await placeBidListing(placeBid);
 }
